e7cdf079a749d5770903690aeb9b09fc5c8eb330
Caleb McDonald <caleb.scott.mcdonald@gmail.com>
2024-01-04 13:37:53 -0600

    practicing advanced queries in django

--- leagues/views.py
+++ leagues/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import League, Team, Player
-
+from django.db.models import Q
 from . import team_maker
 
 def index(request):
@@ -8,6 +8,21 @@ def index(request):
 		"leagues": League.objects.all(),
 		"teams": Team.objects.all(),
 		"players": Player.objects.all(),
+		"baseball_leagues": League.objects.filter(sport='Baseball'),
+		"women_leagues": League.objects.filter(name__icontains="Women"),
+		"hockey_leagues": League.objects.filter(sport__icontains="Hockey"),
+		"not_football_leagues": League.objects.exclude(sport="Football"),
+		"conferences": League.objects.filter(name__icontains="Conference"),
+		"atlantic_leagues": League.objects.filter(name__icontains="Atlantic"),
+		"dallas_teams": Team.objects.filter(location="Dallas"),
+		"raptors": Team.objects.filter(team_name="Raptors"),
+		"includes_city": Team.objects.filter(location__icontains="City"),
+		"starts_t": Team.objects.filter(team_name__istartswith="T"),
+		"abc_location": Team.objects.order_by('location'),
+		"reverse_abc_name": Team.objects.order_by('-team_name'),
+		"cooper_last": Player.objects.filter(last_name="Cooper"),
+		"joshua_first": Player.objects.filter(first_name="Joshua"),
+		"alexander_or_wyatt": Player.objects.filter(Q(first_name="Alexander") | Q(first_name="Wyatt")),
 	}
 	return render(request, "leagues/index.html", context)
 
--- sports_orm_two/settings.py
+++ sports_orm_two/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5e$o$k7_!w_dr%gwud)@+)jgd^wjcc%o(5e95s6ikcsgj)np!k'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
